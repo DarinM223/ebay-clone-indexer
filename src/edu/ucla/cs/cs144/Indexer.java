c59d5f7c3a5e64294a6ba64132c8ee516767a18e
xkevintong <kevin_tong100@hotmail.com>
2015-02-06 17:43:21 -0800

    Started working

--- src/edu/ucla/cs/cs144/Indexer.java
+++ src/edu/ucla/cs/cs144/Indexer.java
@@ -29,8 +29,8 @@ public class Indexer {
     }
  
     public void rebuildIndexes() {
-
         Connection conn = null;
+        IndexWriter iw = null;
 
         // create a connection to the database to retrieve Items from MySQL
 	    try {
@@ -39,26 +39,16 @@ public class Indexer {
 	        System.out.println(ex);
 	    }
 
+        //create lucene indexwriter
+        try {
+            iw = new IndexWriter(System.getenv("LUCENE_INDEX"),
+                new StandardAnalyzer(), true);
+        }
+        catch (IOException e) {
+            System.out.println(ex);
+        }
 
-	/*
-	 * Add your code here to retrieve Items using the connection
-	 * and add corresponding entries to your Lucene inverted indexes.
-         *
-         * You will have to use JDBC API to retrieve MySQL data from Java.
-         * Read our tutorial on JDBC if you do not know how to use JDBC.
-         *
-         * You will also have to use Lucene IndexWriter and Document
-         * classes to create an index and populate it with Items data.
-         * Read our tutorial on Lucene as well if you don't know how.
-         *
-         * As part of this development, you may want to add 
-         * new methods and create additional Java classes. 
-         * If you create new classes, make sure that
-         * the classes become part of "edu.ucla.cs.cs144" package
-         * and place your class source files at src/edu/ucla/cs/cs144/.
-	 * 
-	 */
-
+        
 
         // close the database connection
 	    try {
